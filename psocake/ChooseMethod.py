b8893bfd1a563304338c853f4f9c1cfb762ce5b4
Yashas Rao <yashas.rao24@gmail.com>
2018-01-09 23:42:12 -0800

    Winter Shutdown Changes

--- psocake/ChooseMethod.py
+++ psocake/ChooseMethod.py
@@ -1,22 +1,12 @@
 import GatherCctbxInfo
+from GatherCctbxInfo import Cctbx
+from GatherCrystFelInfo import Crystfel
 
 
-class Process(object):
 
-    def __init__(self):
+class GenerateObject(object):
 
-        self.methodChoice = raw_input("Which software would you like to process your data with? Your options are Cctbx or CrystFEL   ")
+    def __init__(self, cctbx = None, crystfel = None, psana = None, expDir = None):
 
-        self.finalChoice = self.methodChoice.lower()
-
-    @staticmethod
-    def factory(self):
-
-        if self.finalChoice == "cctbx":
-            return Cctbx()
-
-        if self.finalChoice == 'crystfel':
-            return CrystFEL()
-
-        else:
-            print("You have selected an invalid argument")
+        self.cctbx = Cctbx()
+        self.crystfel = Crystfel()
